f5dff8c103c1c1d63c40b9ef16ed6f12077a5b09
Alexandre Poletto <alexandrepoletto@gmail.com>
2016-10-23 14:26:10 -0700

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'))
+});
